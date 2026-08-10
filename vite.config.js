190805c7922b99d5ebfd0f45f27781741bdf4489
zhangwanping <504964656@qq.com>
2026-08-10 09:58:49 +0800

    上传

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Anjoy-jindie-web/',  // 👉 关键就是加上这一行，注意大小写和前后的斜杠
 })
