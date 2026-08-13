6c5a979ae7310a0d7f2caf65cc6dbab2aea7e1f3
zhangwanping <504964656@qq.com>
2026-08-13 09:28:29 +0800

    必填项

--- src/pages/ExpenseForm.jsx
+++ src/pages/ExpenseForm.jsx
@@ -585,7 +585,7 @@ export default function TestPage() {
                         <Col flex="100%">
                             <ProFormTextArea name="reason" label="主题/事由" placeholder="请输入主题/事由" rules={[{ required: true }]} fieldProps={{variant: 'filled', autoSize: { minRows: 3, maxRows: 5 }}}/>
                         </Col>
-                        
+
                         {/*第三类*/}
                         <Col flex="100%">
                             <Title level={5}>结算信息</Title>
@@ -778,7 +778,7 @@ export default function TestPage() {
                                     />
                                 </Col>
                                 <Col flex="20%">
-                                    <ProFormText name="process1" label="分管审核1 " placeholder="请选择或输入分"  rules={[{ required: true }]}
+                                    <ProFormText name="process1" label="分管审核1 " placeholder="请选择或输入分"
                                                  fieldProps={{
                                                      variant: 'filled',
                                                      autoComplete: 'off',
